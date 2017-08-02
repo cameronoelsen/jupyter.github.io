aaf19e33f83859e65de749a7b096c6ada99b0a17
cameronoelsen <cameronoelsen@gmail.com>
2017-08-01 17:37:19 -0700

    Extensions section on JupyterLab page now mobile friendly, disabled extension animation on smaller screens

--- js/tab.js
+++ js/tab.js
@@ -3,31 +3,37 @@ $(document).ready(function() {
    $('div.lab-ext-preview-nav div.lab-ext-preview-nav-item').click(function() {
        var previous_tab_id = $('.lab-ext-preview-nav-item.nav-active').attr('data-tab');
        var current_tab_id = $(this).attr('data-tab');
-       
-       //If the tab is on the right side of the previous tab
-       if (current_tab_id > previous_tab_id) {
-           //Move tab content to left
+
+       //If the screen is larger than 767, animate the extension slide items
+       if ($(window).outerWidth() > 767) {
+           //If the tab is on the right side of the previous tab
+           if (current_tab_id > previous_tab_id) {
+               //Move tab content to left
+               $('#' + current_tab_id).addClass('ext-active');
+               $('#' + previous_tab_id).addClass('move-ext-preview-left-out');
+               $('#' + current_tab_id).addClass('move-ext-preview-right-in');
+               window.setTimeout(function() {
+                   $('#' + previous_tab_id).removeClass('ext-active');
+                   $('#' + previous_tab_id).removeClass('move-ext-preview-left-out');
+                   $('#' + current_tab_id).removeClass('move-ext-preview-right-in');
+               }, 1000);
+           } 
+
+
+           else if (current_tab_id < previous_tab_id){
+               //Move tab content to right
+               $('#' + current_tab_id).addClass('ext-active');
+               $('#' + previous_tab_id).addClass('move-ext-preview-right-out');
+               $('#' + current_tab_id).addClass('move-ext-preview-left-in');
+               window.setTimeout(function() {
+                   $('#' + previous_tab_id).removeClass('ext-active');
+                   $('#' + previous_tab_id).removeClass('move-ext-preview-right-out');
+                   $('#' + current_tab_id).removeClass('move-ext-preview-left-in');
+               }, 300);
+           }
+       } else {
+           $('#' + previous_tab_id).removeClass('ext-active');
            $('#' + current_tab_id).addClass('ext-active');
-           $('#' + previous_tab_id).addClass('move-ext-preview-left-out');
-           $('#' + current_tab_id).addClass('move-ext-preview-right-in');
-           window.setTimeout(function() {
-               $('#' + previous_tab_id).removeClass('ext-active');
-               $('#' + previous_tab_id).removeClass('move-ext-preview-left-out');
-               $('#' + current_tab_id).removeClass('move-ext-preview-right-in');
-           }, 1000);
-       } 
-       
-       
-       else if (current_tab_id < previous_tab_id){
-           //Move tab content to right
-           $('#' + current_tab_id).addClass('ext-active');
-           $('#' + previous_tab_id).addClass('move-ext-preview-right-out');
-           $('#' + current_tab_id).addClass('move-ext-preview-left-in');
-           window.setTimeout(function() {
-               $('#' + previous_tab_id).removeClass('ext-active');
-               $('#' + previous_tab_id).removeClass('move-ext-preview-right-out');
-               $('#' + current_tab_id).removeClass('move-ext-preview-left-in');
-           }, 300);
        }
        
        
