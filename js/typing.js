10e31395e9f5abc2ee26f55366682cff50d767d9
cameronoelsen <cameronoelsen@gmail.com>
2017-08-03 13:54:48 -0700

    First iteration of the animation for collaboration chat section in realtime collaboration section on the JupyterLab page:

--- js/typing.js
+++ js/typing.js
@@ -1,12 +1,15 @@
 // Used for create typing effect on JupyterLab page on Collaboration section
 
-var hasRun = false;
+var tab2HasRun = false,
+    tab3HasRun = false;
+
 $('.lab-collab-preview-nav .lab-collab-preview-nav-item').on('click', function() {
-    if ($(this).data('tab') == 'tab-2' && !hasRun) {
+    if ($(this).data('tab') == 'tab-2' && !tab2HasRun) {
         $('#typeout-cell-1').typist({
             text: '# Analyzing Traffic Data',
             blinkSpeed: .05,
-            cursorStyles: {color: '#2196F3', width: '2px'}
+            cursorStyles: {color: '#2196F3', width: '2px'},
+            speed: 15
         });
         
         $('#typeout-cell-2')
@@ -17,10 +20,21 @@ $('.lab-collab-preview-nav .lab-collab-preview-nav-item').on('click', function()
                 text: 'plt.scatter(df3[‘hour’])',
                 blinkSpeed: .05,
                 cursorStyles: {color: '#BB4430', width: '2px'},
-                speed: 8
+                speed: 10
             });
+                
+        tab2HasRun = true;
+    } else if ($(this).data('tab') == 'tab-3' && !tab3HasRun) {
+        $('#lab-collab-chat-entry-1').addClass('animate-chat-entry-1');
+        $('#lab-collab-chat-entry-2').addClass('animate-chat-entry-2');
         
+        var typeChatEntry2 = function() {
+            $('#typeout-chat-2').typist({
+                text: "Yes! I'll add it to the notebook now."
+            });   
+        };
         
-        hasRun = true;
+        window.setTimeout(typeChatEntry2(), 6000);
+        tab3HasRun = true;
     }
 });
